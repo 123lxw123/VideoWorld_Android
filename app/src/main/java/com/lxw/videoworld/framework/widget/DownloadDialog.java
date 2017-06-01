398f3fa4d9b666254dbdfa6fea5e9c4aedf235ca
lixiongwen <lixiongwen@noah-fund.com>
2017-06-01 18:19:44 +0800

    App 在线升级

--- app/src/main/java/com/lxw/videoworld/framework/widget/DownloadDialog.java
+++ app/src/main/java/com/lxw/videoworld/framework/widget/DownloadDialog.java
@@ -1,24 +1,45 @@
 package com.lxw.videoworld.framework.widget;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.lxw.videoworld.R;
-import com.lxw.videoworld.framework.application.BaseApplication;
 
 /**
  * Created by Zion on 2017/5/30.
  */
 
-public class DownloadDialog extends BaseLoadingDialog{
+public class DownloadDialog extends Dialog{
     protected Context context;
     protected DownloadView downloadView;
 
     public DownloadDialog(@NonNull Context context) {
-        super(context);
+        this(context, true);
+    }
+
+    public DownloadDialog(@NonNull Context context, boolean flag_cancel) {
+        super(context, R.style.TransDialog);
         this.context = context;
+        if(!flag_cancel){
+            OnKeyListener keylistener = new OnKeyListener(){
+                public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            } ;
+            this.setCanceledOnTouchOutside(false);
+            this.setCancelable(false);
+            this.setOnKeyListener(keylistener);
+        }
     }
 
     @Override
