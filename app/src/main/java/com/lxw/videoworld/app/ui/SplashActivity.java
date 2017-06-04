75c5eccebb0a7b8d19fa81d4fde614c641c8dced
lxw <342466322@qq.com>
2017-06-04 20:14:35 +0800

    启动页

---
+++ app/src/main/java/com/lxw/videoworld/app/ui/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.lxw.videoworld.app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lxw.videoworld.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
