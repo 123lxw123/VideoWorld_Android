3e6d6c74e98fa3a3a056f83af2b3a0fd7a2a7188
lxw <342466322@qq.com>
2018-03-10 11:52:12 +0800

    delete

--- thunder/src/main/java/com/xunlei/downloadlib/parameter/BtSubTaskDetail.java
+++
@@ -1,7 +0,0 @@
-package com.xunlei.downloadlib.parameter;
-
-public class BtSubTaskDetail {
-    public int mFileIndex;
-    public boolean mIsSelect;
-    public XLTaskInfo mTaskInfo = new XLTaskInfo();
-}
